25c4fbc7ad79f9a63423e4fba4915d412e56dc46
martblinn <90089382+murumart@users.noreply.github.com>
2026-03-14 11:59:43 +0200

    Notifications disappear on their own

--- scenes/region/ui/Notification.cs
+++ scenes/region/ui/Notification.cs
@@ -7,18 +7,42 @@ public partial class Notification : Control {
 
 	[Export] Label label;
 	[Export] Button dismissButton;
+	[Export] ProgressBar timeProgress;
 
+	public float TimeLimit { get; private set; }
+	public float Time { get; private set; }
 
 	public override void _Ready() {
-		dismissButton.Pressed += QueueFree;
+		dismissButton.Pressed += () => { if (!IsDismissing) Dismiss(); };
 	}
-	
+
 	public void SetText(string text) {
 		label.Text = text;
 	}
 
 	public void SetCallback(Action callback) {
 		if (callback != null) dismissButton.Pressed += callback;
+	}
+
+	public void SetTimeLimit(float timeLimit) {
+		TimeLimit = timeLimit;
+		timeProgress.MaxValue = TimeLimit;
+		timeProgress.Value = TimeLimit;
+	}
+
+	public void IncreaseTime(float delta) {
+		Time += delta;
+		timeProgress.Value = TimeLimit - Time;
+	}
+
+	public bool IsDismissing { get; private set; }
+
+	public void Dismiss() {
+		Debug.Assert(!IsDismissing);
+		IsDismissing = true;
+		var tw = CreateTween().SetTrans(Tween.TransitionType.Cubic);
+		tw.TweenProperty(this, "modulate:a", 0f, 0.2f);
+		tw.TweenCallback(Callable.From(QueueFree));
 	}
 
 }
--- scenes/region/ui/Notifications.cs
+++ scenes/region/ui/Notifications.cs
@@ -1,20 +1,39 @@
 using System;
 using Godot;
+using scenes.autoload;
 
 namespace scenes.region.ui;
 
 public partial class Notifications : VBoxContainer {
 
 	[Export] PackedScene notificationPacked;
-    [Export] Container notificationContainer;
+	[Export] Container notificationContainer;
 
 
-    public void Notify(string text, Action callback = null) {
-        var notif = notificationPacked.Instantiate<Notification>();
-        notif.SetText(text);
-        notif.SetCallback(callback);
-        notificationContainer.AddChild(notif);
-        notificationContainer.MoveChild(notif, 0);
-    }
+	public override void _Process(double delta) {
+		foreach (Node child in notificationContainer.GetChildren()) {
+			if (child is Notification notif && notif.TimeLimit > 0f && !GameMan.Singleton.IsPaused) {
+				notif.IncreaseTime((float)delta);
+				if (notif.Time >= notif.TimeLimit && !notif.IsDismissing) {
+					notif.Dismiss();
+				}
+			}
+		}
+	}
+
+	public Notification Notify(string text, Action callback = null, float timeLimit = 0f) {
+		var notif = notificationPacked.Instantiate<Notification>();
+		notif.SetText(text);
+		notif.SetCallback(callback);
+		if (timeLimit > 0f) notif.SetTimeLimit(timeLimit);
+		notificationContainer.AddChild(notif);
+		notificationContainer.MoveChild(notif, 0);
+
+		var tw = CreateTween().SetParallel().SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		tw.TweenProperty(notif, "modulate:a", 1.0f, 0.3f).From(0.0f);
+		//tw.SetTrans(Tween.TransitionType.Elastic).TweenProperty(notif, "position:y", notif.Position.Y, 0.0f).From(notificationContainer.Size.Y);
+
+		return notif;
+	}
 
 }
